d6814bbb1bc2b5af02768facc4bc198b5369b4cb
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-02-24 15:37:44 -0300

    Caminho banco de dados

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  port: '8111',
+  password: '',
+  database: 'instagramMysqlernClone',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
